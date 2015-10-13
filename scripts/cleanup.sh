d03af566e753565bb89665aeb9f181eeb006014d
Victor Konovalov <vkon@ciklum.com>
2015-10-13 13:24:00 +0300

    adding basic files

---
+++ scripts/cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp/*
+
+# Make sure Udev doesn't block our network
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
+
+# Other cleanups
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+apt-get autoremove -y
---
+++ scripts/dep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Setup the the box. This runs as root
+
+apt-get -y update
+
+apt-get -y install curl wget vim htop
