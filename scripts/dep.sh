f0efadf0f502e48155109ccad3e94a6a2d85ec2d
Victor Konovalov <vkon@ciklum.com>
2015-10-16 20:47:28 +0300

    fixing configs, adding missing files

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #
 # Setup the the box. This runs as root
-
-apt-get -y update
-
+DEV_MODE=0
 DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install curl wget vim
+apt-get -y update >/dev/null
+apt-get -y install curl wget vim htop git
+
+if [[ "$DEV_MODE" -eq "1" ]];then
+echo install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo skipping installing oh-my-zsh...
+fi
